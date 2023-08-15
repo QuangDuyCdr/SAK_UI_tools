170df42e01737bc0701731fe5e5bb432cdfacc9c
QuangDuyCdr <iamdigitalcode@gmail.com>
2023-08-15 13:51:29 +0200

    add stuff

---
+++ src/components/Projects/index.jsx
@@ -0,0 +1,70 @@
+import React, { useState, useLayoutEffect, useRef } from 'react'
+import styles from './style.module.css';
+import Image from 'next/image';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+const projects = [
+    {
+        title: "Custom Layout",
+        src: "customlayout.png"
+    },
+    {
+        title: "Color Palete",
+        src: "color.avif"
+    },
+
+    {
+        title: "Animation",
+        src: "animation.png"
+    },
+
+]
+
+export default function Index() {
+
+    const [selectedProject, setSelectedProject] = useState(0);
+    const container = useRef(null);
+    const imageContainer = useRef(null);
+
+    useLayoutEffect( () => {
+        gsap.registerPlugin(ScrollTrigger);
+        ScrollTrigger.create({
+            trigger: imageContainer.current,
+            pin: true,
+            start: "top-=100px",
+            end: document.body.offsetHeight - window.innerHeight - 50,
+        })
+    }, [])
+
+    return (
+        <div ref={container} className={styles.projects}>
+            <div className={styles.projectDescription}>
+                <div ref={imageContainer} className={styles.imageContainer}>
+                    <Image 
+                        src={`/images/${projects[selectedProject].src}`}
+                        fill={true}
+                        alt="project image"
+                        priority={true}
+                    />
+                </div>
+                <div className={styles.column}>
+                    <p>From basic design components like color paletes and custom container</p>
+                </div>
+                <div className={styles.column}>
+                    <p>to complex thing such as transition and animation for your component. SAK is the way to go </p>
+                </div>
+            </div>
+
+            <div className={styles.projectList}>
+                {
+                    projects.map( (project, index) => {
+                        return <div key={index} onMouseOver={() => {setSelectedProject(index)}} className={styles.projectEl}>
+                            <h2>{project.title}</h2>
+                        </div>
+                    })
+                }
+            </div>
+        </div>
+    )
+}
